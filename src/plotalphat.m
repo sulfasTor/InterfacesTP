168fb1ac7dacaea14a8c3d4a9f5b350f835a8686
Sulf <mtorresagu@insa-lyon.fr>
2017-05-30 17:09:59 +0200

    last jour

--- src/plotalphat.m
+++ src/plotalphat.m
@@ -7,9 +7,10 @@ s = correctionSilice(T);
 
 alpha_moy = m - s;
 
-bound = indexOf(max(T), T);
+bound = find(T == max(T));
+bound = bound(1);
 
-P = polyfit(T(1:bound),alpha_moy(1:bound),2);
+P = polyfit(T(1:bound), alpha_moy(1:bound),1);
 
 p = plot(T, alpha_moy);
 grid on
@@ -17,6 +18,6 @@ title('Alpha lineaire en fonction de la temperature');
 xlabel('Temperature en C');
 ylabel('Alpha lineaire en 1/C');
 
-text(max(T)/3,max(alpha_moy)/10,num2str(P,'Y = %dX^2 + %dX + %d'))
+text(max(T)/3,max(alpha_moy)/5,num2str(P,'Y = %dX + %d'))
 
 end
